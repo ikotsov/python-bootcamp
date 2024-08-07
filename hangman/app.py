3b4b92b964b377522996c1c8abf7e90e030d71a2
gkotsovilis <i.kotsovilis@gmail.com>
2024-08-07 17:34:24 +0300

    feat: add hangman step 2

--- hangman/app.py
+++ hangman/app.py
@@ -1,14 +1,28 @@
 import random
 
+placeholder_char = "_"
+
 word_list = ['aardvark', 'baboon', 'camel']
 
 word = random.choice(word_list)
 print(word)
 
-letter_guessed = input("Guess a letter: \n").lower()
+placeholder = ""
+word_length = len(word)
+
+for position in range(word_length):
+    placeholder += placeholder_char
+
+print(placeholder)
+
+letter_guessed = input("Guess a letter: ").lower()
+
+display = ""
 
 for letter in word:
     if letter == letter_guessed:
-        print("Right")
+        display += letter
     else:
-        print("Wrong")
+        display += placeholder_char
+
+print(display)
