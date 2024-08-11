0d6877a3b696f06104d0cb32dd39a0b80d14d5b6
gkotsovilis <i.kotsovilis@gmail.com>
2024-08-11 10:57:15 +0300

    refactor: let user choose any number

--- caesar-cipher/app.py
+++ caesar-cipher/app.py
@@ -1,10 +1,9 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
-alphabet_length = len(alphabet)
 
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
 text = input("Type your message\n").lower()
-shift = int(input("Type a shift number between 1 and 26\n").lower())
+shift = int(input("Type a shift number\n").lower())
 
 
 def encrypt(original_text, shift_amount):
@@ -12,11 +11,9 @@ def encrypt(original_text, shift_amount):
     for letter in original_text:
         current_position = alphabet.index(letter)
         shifted_position = current_position + shift_amount
+        final_position = shifted_position % len(alphabet)
 
-        if shifted_position >= alphabet_length:
-            encrypted_text += alphabet[shifted_position - alphabet_length]
-        else:
-            encrypted_text += alphabet[shifted_position]
+        encrypted_text += alphabet[final_position]
 
     print(f"Here is the encoded result {encrypted_text}")
 
