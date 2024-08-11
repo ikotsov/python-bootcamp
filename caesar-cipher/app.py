52545e11973017872fdd8951cfde84c281adf58b
gkotsovilis <i.kotsovilis@gmail.com>
2024-08-11 11:37:38 +0300

    refactor: reorganize code

--- caesar-cipher/app.py
+++ caesar-cipher/app.py
@@ -1,10 +1,6 @@
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
             'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
-text = input("Type your message\n").lower()
-shift = int(input("Type a shift number\n").lower())
-
 
 def caesar(original_text, shift_amount, encode_or_decode):
     if encode_or_decode == 'decode':
@@ -21,5 +17,9 @@ def caesar(original_text, shift_amount, encode_or_decode):
 
     print(f"Here is the {encode_or_decode}d result {output_text}")
 
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt\n").lower()
+text = input("Type your message\n").lower()
+shift = int(input("Type a shift number\n").lower())
 
 caesar(text, shift, direction)
