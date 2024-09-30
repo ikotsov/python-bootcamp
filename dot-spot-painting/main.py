de30dd1a82cb22cbed78af15bb0ddc12ec392a7a
gkotsovilis <i.kotsovilis@gmail.com>
2024-09-30 10:57:10 +0300

    feat: random walk

--- dot-spot-painting/main.py
+++ dot-spot-painting/main.py
@@ -1,14 +1,23 @@
-from turtle import Turtle
+import turtle as t
+import random
 
-turtle = Turtle()
-
-
-def draw_shape(num_of_sides):
-    angle = 360 / num_of_sides
-    for _ in range(num_of_sides):
-        turtle.right(angle)
-        turtle.forward(100)
+t.colormode(255)
 
 
-for num_of_sides in range(3, 11):
-    draw_shape(num_of_sides)
+def get_random_color():
+    red = random.randint(0, 255)
+    green = random.randint(0, 255)
+    blue = random.randint(0, 255)
+    random_color = (red, green, blue)
+    return random_color
+
+
+turtle = t.Turtle()
+turtle.pensize(15)
+
+orientation = [0, 90, 180, 270]
+
+for _ in range(200):
+    turtle.color(get_random_color())
+    turtle.setheading(random.choice(orientation))
+    turtle.forward(30)
