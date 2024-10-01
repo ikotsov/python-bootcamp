3497847b6e28484f6cc3369b1125c84fbf6ac99a
gkotsovilis <i.kotsovilis@gmail.com>
2024-10-01 20:25:17 +0300

    feat: add painting creation

--- dot-spot-painting/main.py
+++ dot-spot-painting/main.py
@@ -1,8 +1,10 @@
 import colorgram
+import turtle as t
+import random
 
 colors = colorgram.extract('painting.webp', 30)
 
-rgb_colors= []
+rgb_colors = []
 
 for color in colors:
     red = color.rgb.r
@@ -10,4 +12,23 @@ for color in colors:
     blue = color.rgb.b
     rgb_colors.append((red, green, blue))
 
-print(rgb_colors)
+t.colormode(255)
+t.penup()
+t.hideturtle()
+t.speed("fastest")
+
+t.setx(-270)
+t.sety(-270)
+
+for dot_count in range(1, 101):
+    random_color = random.choice(rgb_colors)
+
+    t.dot(20, random_color)
+    t.forward(50)
+
+    if dot_count % 10 == 0:
+        t.sety(t.ycor() + 50)
+        t.setx(-270)
+
+screen = t.Screen()
+screen.exitonclick()
